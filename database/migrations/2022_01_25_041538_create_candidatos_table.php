e164dc14a9c04ede68b63017258bd9230283533f
master <71289299+Taniguchi23@users.noreply.github.com>
2022-01-24 23:34:17 -0500

    migrate y login

---
+++ database/migrations/2022_01_25_041538_create_candidatos_table.php
@@ -0,0 +1,36 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateCandidatosTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('candidatos', function (Blueprint $table) {
+            $table->id();
+            $table->string('nombre');
+            $table->foreignId('distrito_id')->references('id')->on('distritos');
+            $table->foreignId('partido_id')->references('id')->on('partidos');
+            $table->string('imagen',65)->nullable();
+            $table->date('fecha')->nullable();
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('candidatos');
+    }
+}
---
+++ database/migrations/2022_01_25_042713_create_datos_table.php
@@ -0,0 +1,35 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateDatosTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('datos', function (Blueprint $table) {
+            $table->id();
+            $table->foreignId('candidato_id')->references('id')->on('candidatos');
+            $table->foreignId('evento_id')->references('id')->on('eventos');
+            $table->integer('votos');
+            $table->date('fecha')->nullable();
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('datos');
+    }
+}
