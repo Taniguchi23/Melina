f04eda3087fe915034cb07e4c3e9cba0955b0144
MiguelRoot <miguelkillki@gmail.com>
2022-01-30 02:13:53 -0500

    list votos v3

--- resources/views/web/index.blade.php
+++ resources/views/web/index.blade.php
@@ -45,7 +45,7 @@
                         <a href="#" class="fh5co_tagg">Social</a>
                         <a href="#" class="fh5co_tagg">Three</a>
                     </div>
-                    <div>
+                    {{-- <div>
                         <div class="fh5co_heading fh5co_heading_border_bottom pt-3 py-2 mb-4">Most Popular</div>
                     </div>
                     <div class="row pb-3">
@@ -82,7 +82,7 @@
                             <div class="most_fh5co_treding_font"> Magna aliqua ut enim ad minim veniam quis nostrud.</div>
                             <div class="most_fh5co_treding_font_123"> April 18, 2016</div>
                         </div>
-                    </div>
+                    </div> --}}
                 </div>
             </div>
             <div class="row mx-0">
